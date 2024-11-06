b5731d26e7768d76f371d2d784577afe2197492b
miquelcabot <miquel.cabot@gmail.com>
2024-11-06 16:26:31 +0100

    fix clippy issues in main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use bitcoin::FieldElement;
 use bitcoin::Point;
 use bitcoin::PrivateKey;
 use bitcoin::S256Point;
-use bitcoin::Signature;
 use num_bigint::BigUint;
 
 fn main() {
@@ -74,12 +73,12 @@ fn main() {
     println!("{}", s256point.get_point().clone() * n);
 
     // Verifying signature
-    let z = b"bc62d4b80d9e36da29c16c5d4d9f11731f36052c72401a76c23c0fb5a9b74423";
-    let r = b"37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6";
-    let s = b"8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec";
+    let _z = b"bc62d4b80d9e36da29c16c5d4d9f11731f36052c72401a76c23c0fb5a9b74423";
+    let _r = b"37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6";
+    let _s = b"8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec";
     let px = b"04519fac3d910ca7e7138f7013706f619fa8f033e6ec6e09370ea38cee6a7574";
     let py = b"82b51eab8c27c66e26c858a079bcdf4f1ada34cec420cafc7eac1a42216fb6c4";
-    let point = S256Point::new(Some(px), Some(py));
+    let _point = S256Point::new(Some(px), Some(py));
 
     // Private key
     let private_key =
