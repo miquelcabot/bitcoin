ae031182bc1fb48a2cc386a8d46fdccce4cffe62
miquelcabot <miquel.cabot@gmail.com>
2025-02-16 14:59:56 +0100

    refactor: simplify module exports in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
 mod cryptography;
 
-pub use cryptography::hash256;
-pub use cryptography::FieldElement;
-pub use cryptography::Point;
-pub use cryptography::PrivateKey;
-pub use cryptography::S256Point;
-pub use cryptography::Signature;
+pub use cryptography::*;
 pub use num_bigint::BigUint;
