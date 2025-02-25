6c6c19ab5dc388073d92986b7cfd4d39318c9fd2
miquelcabot <miquel.cabot@gmail.com>
2025-02-26 00:08:40 +0100

    feat: implement DER encoding for Signature and add corresponding test

--- src/cryptography/signature.rs
+++ src/cryptography/signature.rs
@@ -30,6 +30,42 @@ impl Signature {
     }
 }
 
+impl Signature {
+    pub fn der(&self) -> Vec<u8> {
+        fn encode_integer(value: &BigUint) -> Vec<u8> {
+            let mut bin = value.to_bytes_be(); // Convert BigUint to bytes (Big Endian)
+            while bin.first() == Some(&0) {
+                // Remove `0x00` bytes at the beginning
+                bin.remove(0);
+            }
+            if bin.first().is_some_and(|&b| b & 0x80 != 0) {
+                // If the first byte has the most significant bit set, add a `0x00`
+                bin.insert(0, 0x00);
+            }
+            let mut encoded = vec![0x02, bin.len() as u8]; // Prefix for INTEGER
+            encoded.extend(bin);
+            encoded
+        }
+
+        let mut result = encode_integer(&self.r);
+        result.extend(encode_integer(&self.s));
+
+        let mut der_encoded = vec![0x30, result.len() as u8]; // SEQUENCE prefix
+        der_encoded.extend(result);
+
+        der_encoded
+    }
+
+    pub fn der_str(&self) -> String {
+        let der = self.der();
+        let mut der_str = String::new();
+        for byte in der {
+            der_str.push_str(&format!("{:02x}", byte));
+        }
+        der_str
+    }
+}
+
 impl Display for Signature {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "Signature({},{})", self.r, self.s)
@@ -84,6 +120,29 @@ mod tests {
         let private_key3 = PrivateKey::new(secret2)?;
         assert_eq!(private_key1, private_key2);
         assert_ne!(private_key1, private_key3);
+        Ok(())
+    }
+
+    #[test]
+    fn test_signature_der() -> Result<()> {
+        let r = BigUint::parse_bytes(
+            b"37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6",
+            16,
+        )
+        .unwrap();
+        let s = BigUint::parse_bytes(
+            b"8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec",
+            16,
+        )
+        .unwrap();
+
+        let signature = Signature::new(r, s);
+        let der_str = signature.der_str();
+        assert_eq!(
+            der_str,
+            "3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c60221008ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec"
+        );
+
         Ok(())
     }
 }
