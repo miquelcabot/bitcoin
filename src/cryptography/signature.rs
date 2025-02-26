84c96814dc7a673a197662c79e9003a48eb295f7
miquelcabot <miquel.cabot@gmail.com>
2025-02-26 08:38:19 +0100

    refactor: format DER string in signature test for improved readability

--- src/cryptography/signature.rs
+++ src/cryptography/signature.rs
@@ -140,7 +140,8 @@ mod tests {
         let der_str = signature.der_str();
         assert_eq!(
             der_str,
-            "3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c60221008ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec"
+            "3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c\
+             60221008ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec"
         );
 
         Ok(())
